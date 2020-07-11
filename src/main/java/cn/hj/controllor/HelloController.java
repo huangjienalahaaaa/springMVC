ce68eb88ca47c0763460218276295f7646128ef5
huangjie <137612270@qq.com>
2020-07-11 13:11:58 +0800

    1

--- src/main/java/cn/hj/controllor/HelloController.java
+++ src/main/java/cn/hj/controllor/HelloController.java
@@ -12,8 +12,11 @@ RequestMapping属性解说：
 3. params(限制请求参数的条件。它支持简单的表达式-> 要求请求参数的key和value必须和配置一模一样)。
     例如：
         params = {"accountName"},表示请求参数必须有accountName
+        params = {"accountName=123"},表示请求参数accountName必须要有，且必须等于123
         params = {"money!100"},表示请求参数中money不能是100
-4. headers（限制请求头的条件）
+4. headers（限制请求头的条件）：
+    例如：
+    headers={"Accept"}  表示，请求头headers中必须要有Accept属性
 注意：
 以上四个属性只要出现2个或以上时，他们的关系是与的关系。
 */
