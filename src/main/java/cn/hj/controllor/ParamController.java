083b1d65acc08913d9b83bfd80c7ba1fe7ac54c2
huangjie <137612270@qq.com>
2020-07-13 10:24:17 +0800

    f

--- src/main/java/cn/hj/controllor/ParamController.java
+++ src/main/java/cn/hj/controllor/ParamController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ParamController {
     @RequestMapping("/testParam")
     public  String testParam(String username,String password){
+//        一般情况下，零散的属性是可以这样子做的，但是如果如果url跟的参数比较多的话，一般都是封装成java Bean来做的,请看v2-1
         System.out.println(username);
         System.out.println(password);
         return "success";
