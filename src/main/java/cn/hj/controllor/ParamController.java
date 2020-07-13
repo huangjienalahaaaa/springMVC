ea2faaf1713ebe08dca5da21ae45cd9d3ea327b2
huangjie <137612270@qq.com>
2020-07-13 10:44:00 +0800

    f

--- src/main/java/cn/hj/controllor/ParamController.java
+++ src/main/java/cn/hj/controllor/ParamController.java
@@ -1,18 +1,18 @@
 package cn.hj.controllor;
 
+import cn.hj.domain.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-//请求参数绑定
+
 @Controller
 @RequestMapping("/params")
 public class ParamController {
-    @RequestMapping("/testParam")
-    public  String testParam(String username,String password){
-//        一般情况下，零散的属性是可以这样子做的，但是如果如果url跟的参数比较多的话，一般都是封装成java Bean来做的,请看v2-1
-        System.out.println(username);
-        System.out.println(password);
+//    请求参数绑定，把数据封装到javaBean的类中
+    @RequestMapping("/saveAccount")
+    public  String saveAccount(Account account){
+        System.out.println(account);
         return "success";
     }
 }
--- src/main/webapp/params.jsp
+++ src/main/webapp/params.jsp
@@ -4,7 +4,12 @@
     <title>Title</title>
 </head>
 <body>
-<%--请求参数绑定--%>
 
+<form action="params/saveAccount" method="post">
+    姓名：<input type="text" name="username"><br/>
+    密码：<input type="text" name="password"><br/>
+    金额：<input type="text" name="money"><br/>
+    <input type="submit" value="提交">
+</form>
 </body>
 </html>
