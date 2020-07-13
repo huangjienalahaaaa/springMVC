ab587edccec0819e1395cfafba64a735c62ea6f2
huangjie <137612270@qq.com>
2020-07-13 10:08:19 +0800

    f

--- src/main/java/cn/hj/controllor/HelloController.java
+++ src/main/java/cn/hj/controllor/HelloController.java
@@ -2,24 +2,9 @@ package cn.hj.controllor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//控制器类
 @Controller
-@RequestMapping(path="user") /*
-RequestMapping属性解说：
-1. = @RequestMapping(value="user") =@RequestMapping("user")
-2. method（请求方式）：
-    @RequestMapping(path="user",method={RequestMethod.POST})
-3. params(限制请求参数的条件。它支持简单的表达式-> 要求请求参数的key和value必须和配置一模一样)。
-    例如：
-        params = {"accountName"},表示请求参数必须有accountName
-        params = {"accountName=123"},表示请求参数accountName必须要有，且必须等于123
-        params = {"money!100"},表示请求参数中money不能是100
-4. headers（限制请求头的条件）：
-    例如：
-    headers={"Accept"}  表示，请求头headers中必须要有Accept属性
-注意：
-以上四个属性只要出现2个或以上时，他们的关系是与的关系。
-*/
+@RequestMapping(path="user")
+
 public class HelloController {
     @RequestMapping(path="/hello")
     public String sayHello(){
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,7 +5,6 @@
 </head>
 <body>
     <h3>入门程序</h3>
-<%--    <a href="hello">点我</a>--%>
     <a href="user/hello">点我</a>
     <a href="user/testRequestMapping">RequestMapping注解</a>
 </body>
