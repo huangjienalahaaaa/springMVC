aaf09997a43339bd59e46e1c8f70ea7a90f7c432
huangjie <137612270@qq.com>
2020-07-15 15:28:06 +0800

    f

--- src/main/java/cn/hj/controllor/AnnoController.java
+++ src/main/java/cn/hj/controllor/AnnoController.java
@@ -2,6 +2,7 @@ package cn.hj.controllor;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,10 +13,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/anno")
 public class AnnoController {
 
-//获取Cookie的值
-    @RequestMapping("/testCookieValue")
-    public String testCookieValue(@CookieValue(value="JSESSSIONID") String cookieValue){
-        System.out.println(cookieValue);
+    @RequestMapping("/testModelAttribute")
+    public String testModelAttribute(){
+        System.out.println("testModelAttribute执行了");
         return  "success";
     }
+
+    //放在方法上，会先执行下面这个方法。再执行上面的testModelAttribute()方法
+    @ModelAttribute
+    public void showUser(){
+        System.out.println("showUser方法执行了");
+    }
+
 }
