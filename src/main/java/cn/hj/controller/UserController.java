5bfd4d0758f1742cd3e8fb60aaa5fc19ee21ba83
huangjie <137612270@qq.com>
2020-07-18 11:07:25 +0800

    f

--- src/main/java/cn/hj/controller/UserController.java
+++ src/main/java/cn/hj/controller/UserController.java
@@ -4,6 +4,7 @@ package cn.hj.controller;
 import cn.hj.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,13 +16,12 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @RequestMapping("/testForwardOrRedirect")
-    public String testForwardOrRedirect(){
 
-    //1. 请求转发(使用关键字的方式，这种方式不能使用视图解析器)，下面是固定格式
-    //return "forward:/WEB-INF/pages/success.jsp";
-
-    //2. 重定向(还是一样的问题，重定向不能访问/WEB-INF目录以及下面的所有文件，但是可以访问webapp/index.jsp文件)
-        return "redirect:/index.jsp";
+/* 模拟异步请求响应:
+  @RequestBody:获取请求体
+ */
+    @RequestMapping("/testAjax")
+    public void testAjax(@RequestBody String body){
+        System.out.println(body);
     }
 }
