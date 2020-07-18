155e5377dae582dacfc221360b445416fe2fc3a1
huangjie <137612270@qq.com>
2020-07-18 09:18:13 +0800

    f

--- src/main/java/cn/hj/controller/UserController.java
+++ src/main/java/cn/hj/controller/UserController.java
@@ -6,24 +6,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 返回值类型是void
+ *
+ * 1. 请求转发和重定向的区别：
+ *      1. 请求转发：是一次请求，不用编写项目的名称
+ *      2. 重定向： 是2次请求，所以要写项目的名字
+ */
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @RequestMapping("/testString")
-    public String testString(Model model){
+    @RequestMapping("/testVoid")
+    public void testVoid(HttpServletRequest request, HttpServletResponse response) throws Exception{
 
-        System.out.println("testString执行了...");
+// 1.编写请求转发的程序
+//        request.getRequestDispatcher("/WEB-INF/pages/success.jsp").forward(request,response);
 
-//        模拟从数据库中查询出User对象
-        User user = new User();
-        user.setUsername("妹妹");
-        user.setPassword("123");
-        user.setAge(30);
 
-    //使用model对象存储数据
-        model.addAttribute("user",user);
+// 2. 重定向(相当于重新发送了一个请求)，但是请求转发程序可以直接访问/WEB-INF/下的文件，但是重定向不可以、
+//       response.sendRedirect(request.getContextPath()+"/WEB-INF/pages/success.jsp"); x
+//       response.sendRedirect(request.getContextPath()+"/index.jsp");
 
-    //去success.jsp将值取出来
-        return "success";
+/* 3. 直接进行响应（
+ 上面都是：直接跳到jsp，然后通过tomcat服务器帮你生成html文件，最终响应给用户。
+    但是现在又这么一个需求：
+    通过输出流，直接把数据响应给浏览器
+）
+ */
+// 设置中文乱码
+    response.setCharacterEncoding("UTF-8");
+    response.setContentType("text/html;charset=UTF-8");
+
+    response.getWriter().print("hello");
+
+     return; //不想让后面的代码执行
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,8 +4,7 @@
     <title>Title</title>
 </head>
 <body>
-
-<a href="user/testString">testString</a>
-
+<a href="user/testVoid">testVoid</a>
+<h3>重定向过来了又</h3>
 </body>
 </html>
