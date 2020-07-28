50bdb5ec33478098f17434fb8ab9cad8bd2b2d99
huangjie <137612270@qq.com>
2020-07-28 10:57:42 +0800

    f

--- src/main/java/cn/hj/controller/UserController.java
+++ src/main/java/cn/hj/controller/UserController.java
@@ -1,10 +1,11 @@
 package cn.hj.controller;
 
-//import com.sun.jersey.api.client.Client;
-//import com.sun.jersey.api.client.WebResource;
-//import org.apache.commons.fileupload.FileItem;
-//import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-//import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,29 +20,24 @@ import java.util.UUID;
 public class UserController {
 //    SpringMVC文件上传
     @RequestMapping("fileUpload2")
-    public String fileUpload2(HttpServletRequest request, MultipartFile upload){
+    public String fileUpload2( MultipartFile upload){
+
+        //定义上传文件服务器路径
+        String path = "http://localhost:9090/uploads/"；
 
 
-        String path = request.getSession().getServletContext().getRealPath("/uploads/");
-        File file = new File(path);
-        if(!file.exists()){
-            // 创建该文件夹
-            file.mkdirs();
-        }
+         String filename = upload.getOriginalFilename();
+         String uuid = UUID.randomUUID().toString().replace("-","");
+         filename = uuid+"_"+filename;
 
+        //创建客户端的对象
+        Client client = Client.create();
 
+        //和图片服务器进行连接
+        WebResource webResource = client.resource(path+filename);
 
-            // 获取上传文件的名称
-            String filename = upload.getOriginalFilename();
-
-
-            String uuid = UUID.randomUUID().toString().replace("-","");
-            filename = uuid+"_"+filename;
-
-
-            //完成文件上传
-        upload.transferTo(new File(path,filename));
-
+        //上传文件
+        webResource.put(upload.getBytes());
 
         return "success";
     }
