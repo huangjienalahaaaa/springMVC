58759cb7367b6587fe08c4ffc5851011bd9843f3
huangjie <137612270@qq.com>
2020-07-28 10:19:54 +0800

    f

--- src/main/java/cn/hj/controller/UserController.java
+++ src/main/java/cn/hj/controller/UserController.java
@@ -17,50 +17,30 @@ import java.util.UUID;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-//    文件上传
-    @RequestMapping("fileUpload1")
-    public String fileUpload1(HttpServletRequest request, MultipartFile upload){
-        System.out.println("springmvc文件上传...");
+//    SpringMVC文件上传
+    @RequestMapping("fileUpload2")
+    public String fileUpload2(HttpServletRequest request, MultipartFile upload){
 
-        // 使用fileupload组件完成文件上传
-        // 上传的位置
+
         String path = request.getSession().getServletContext().getRealPath("/uploads/");
-        // 判断，该路径是否存在
         File file = new File(path);
         if(!file.exists()){
             // 创建该文件夹
             file.mkdirs();
         }
 
-        //解析request对象，获取上传文件项
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        ServletFileUpload upload = new ServletFileUpload(factory);
-
-        // 解析request
-        List<FileItem> items = upload.parseRequest(request);
-        //遍历
-        for(FileItem item:items){
-            if(item.isFormField()){
-                //说明普通表单项
-            }else{
-                //说明上传文件项
-                // 获取上传文件的名称
-                String filename = item.getName();
-
-                //把文件的名称设置成唯一值.uuid
-                String uuid = UUID.randomUUID().toString().replace("-","");
-                filename = uuid+"_"+filename;
 
 
-                //完成文件上传
-                item.write(new File(path,filename));
-                //删除临时文件
-                item.delete();
-            }
-        }
+            // 获取上传文件的名称
+            String filename = upload.getOriginalFilename();
 
 
+            String uuid = UUID.randomUUID().toString().replace("-","");
+            filename = uuid+"_"+filename;
 
+
+            //完成文件上传
+        upload.transferTo(new File(path,filename));
 
 
         return "success";
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,7 +5,7 @@
 <body>
     <h3>文件上传</h3>
 
-    <form action="/user/fileUpload1" method="post" enctype="multipart/form-data">
+    <form action="/user/fileUpload2" method="post" enctype="multipart/form-data">
         选择文件：<input type="file" name="upload"><br/>
         <input type="submit" value="上传">
     </form>
